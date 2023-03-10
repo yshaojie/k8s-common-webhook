59fdfeaa72af940b7a07a74b29f83fad0fa7432b
岳少杰 <shaojie.yue@beyonca.com>
2023-03-11 01:01:11 +0800

    init

--- api/v1/pod_webhook.go
+++ api/v1/pod_webhook.go
@@ -5,6 +5,7 @@ import (
 	corev1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/util/json"
 	"net/http"
+	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/webhook/admission"
 )
@@ -13,6 +14,46 @@ type PodAnnotator struct {
 	Client  client.Client
 	decoder *admission.Decoder
 }
+
+var (
+	log           = ctrl.Log.WithName("webhook")
+	commonEnvVars = []corev1.EnvVar{{
+		Name: "K8S_WORK_NAME",
+		ValueFrom: &corev1.EnvVarSource{
+			FieldRef: &corev1.ObjectFieldSelector{
+				FieldPath: "spec.nodeName",
+			},
+		},
+	}, {
+		Name: "K8S_POD_NAME",
+		ValueFrom: &corev1.EnvVarSource{
+			FieldRef: &corev1.ObjectFieldSelector{
+				FieldPath: "metadata.name",
+			},
+		},
+	}, {
+		Name: "K8S_POD_NAMESPACE",
+		ValueFrom: &corev1.EnvVarSource{
+			FieldRef: &corev1.ObjectFieldSelector{
+				FieldPath: "metadata.namespace",
+			},
+		},
+	}, {
+		Name: "K8S_POD_IP",
+		ValueFrom: &corev1.EnvVarSource{
+			FieldRef: &corev1.ObjectFieldSelector{
+				FieldPath: "status.podIP",
+			},
+		},
+	}, {
+		Name: "K8S_WORK_IP",
+		ValueFrom: &corev1.EnvVarSource{
+			FieldRef: &corev1.ObjectFieldSelector{
+				FieldPath: "status.hostIP",
+			},
+		},
+	}}
+)
 
 //+kubebuilder:webhook:path=/mutate-v1-pod,mutating=true,sideEffects=NoneOnDryRun,admissionReviewVersions=v1,failurePolicy=fail,groups="",resources=pods,verbs=create;update,versions=v1,name=xy.meteor.io
 
@@ -24,10 +65,43 @@ func (a *PodAnnotator) Handle(ctx context.Context, req admission.Request) admiss
 	}
 
 	//在 pod 中修改字段
-
+	log.Info("pod ....", "123")
+	mutatePod(pod)
 	marshaledPod, err := json.Marshal(pod)
 	if err != nil {
 		return admission.Errored(http.StatusInternalServerError, err)
 	}
 	return admission.PatchResponseFromRaw(req.Object.Raw, marshaledPod)
+}
+
+func mutatePod(pod *corev1.Pod) error {
+	containers := pod.Spec.Containers
+	fillCommonEnvVars(containers)
+	initContainers := pod.Spec.InitContainers
+	fillCommonEnvVars(initContainers)
+	return nil
+}
+
+func fillCommonEnvVars(containers []corev1.Container) error {
+	for i, container := range containers {
+		envVars := container.Env
+		for _, commonEnvVar := range commonEnvVars {
+			if hasEnvVar(container, commonEnvVar) {
+				continue
+			}
+			envVars = append(envVars, commonEnvVar)
+		}
+		containers[i].Env = envVars
+	}
+
+	return nil
+}
+
+func hasEnvVar(container corev1.Container, checkEnvVar corev1.EnvVar) bool {
+	for _, envVar := range container.Env {
+		if envVar.Name == checkEnvVar.Name {
+			return true
+		}
+	}
+	return false
 }
--- upgrade.sh
+++ upgrade.sh
@@ -1,3 +1,5 @@
-make docker-build IMG=common-webhook:v1
-kind load docker-image common-webhook:v1
-make deploy IMG=common-webhook:v1
+tag=$(date '+%m%d%H%M%S')
+make docker-build IMG=common-webhook:${tag}
+kind load docker-image common-webhook:${tag}
+sleep 3
+make deploy IMG=common-webhook:${tag}
